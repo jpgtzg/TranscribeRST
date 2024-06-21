c612b015ffb37f906162a9fde599b5cec89f2d57
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-06-20 19:18:23 -0600

    Moved algorithm to folder

---
+++ algorithm/audio.py
@@ -0,0 +1,35 @@
+# Written by Juan Pablo Gutiérrez
+# 26 05 2024
+
+import whisper
+from pydub import AudioSegment
+
+# Loads the model and transcribes the audio file
+def get_transcription(audio_file):    
+    model = whisper.load_model('base')
+    
+    # Convert the audio file to a format that Whisper can process
+    audio = AudioSegment.from_file(audio_file, format='mp3')
+    audio.export('temp.mp3', format='mp3')
+
+    # Load the audio file for Whisper
+    audio_data = whisper.load_audio('temp.mp3')
+
+    options = {
+        "language": "en",
+        "task": "translate" 
+    }
+
+    result = whisper.transcribe(model, audio_data, **options, fp16=False)
+
+    text_value = result.get('text', '')
+
+    text_string = str(text_value)
+
+    with open('result.txt', 'w') as file:
+        file.write(text_string)
+
+    print(text_string)
+
+    return text_string
+
